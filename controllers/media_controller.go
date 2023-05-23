97265a27779ff20c864dcfb56f9f3764c669e36e
dgsaltarin <dgsaltarin@gmail.com>
2023-05-23 08:48:26 -0500

    added helpers ans functon that return row item information

--- controllers/media_controller.go
+++ controllers/media_controller.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/dgsaltarin/SharedBitesBackend/helpers"
 	"github.com/dgsaltarin/SharedBitesBackend/services"
 
 	"github.com/gin-gonic/gin"
@@ -29,28 +30,43 @@ func UploadImage() gin.HandlerFunc {
 			return
 		}
 
-		src, err := image.Open()
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		defer src.Close()
-
-		data := make([]byte, image.Size)
-		_, err = src.Read(data)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
+		data, err := helpers.DecodeImage(image)
 
 		session := services.RekognitionSession()
 
 		result := services.DetectLabels(session, data)
+
+		output, err := json.Marshal(result)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"result": string(output),
+		})
+	}
+}
+
+func UploadTextTract() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		image, err := c.FormFile("file")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		data, err := helpers.DecodeImage(image)
+
+		services.UploadImages3(services.AWSSession(), data, image.Filename)
+
+		session := services.TextTrackSesson()
+
+		result := services.DetectText(session, image.Filename)
 
 		output, err := json.Marshal(result)
 		if err != nil {
