792e04a819c7853ed379c1cc4512ee741b171591
daniel saltarin castillo <dgsaltarin@gmail.com>
2024-06-13 20:29:04 -0500

    added user repository dependency

--- internal/vertical/users/application/services/user_service.go
+++ internal/vertical/users/application/services/user_service.go
@@ -3,15 +3,25 @@ package services
 import (
 	services "github.com/dgsaltarin/SharedBitesBackend/internal/vertical/users/application"
 	"github.com/dgsaltarin/SharedBitesBackend/internal/vertical/users/domain/entity"
+	"github.com/dgsaltarin/SharedBitesBackend/internal/vertical/users/domain/repository"
 )
 
-type userService struct{}
+type userService struct {
+	repository repository.UserRepository
+}
 
-func NewUserService() services.UserService {
-	return &userService{}
+func NewUserService(repository repository.UserRepository) services.UserService {
+	return &userService{
+		repository: repository,
+	}
 }
 
 func (u *userService) SignUp(user entity.User) error {
+	err := u.repository.UpsertUser(&user)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
