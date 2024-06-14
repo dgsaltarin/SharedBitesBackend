1f202f07b9474183c4e8eca26a5c9a8eae951bf1
daniel saltarin castillo <dgsaltarin@gmail.com>
2024-06-14 10:21:03 -0500

    added hash function for password and new user funtion

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21
 toolchain go1.21.9
 
 require (
+	github.com/google/uuid v1.6.0
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.0
 	go.uber.org/dig v1.17.1
@@ -23,7 +24,6 @@ require (
 	github.com/go-openapi/spec v0.20.4 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
--- internal/vertical/users/application/services/user_service.go
+++ internal/vertical/users/application/services/user_service.go
@@ -6,7 +6,6 @@ import (
 	services "github.com/dgsaltarin/SharedBitesBackend/internal/vertical/users/application"
 	"github.com/dgsaltarin/SharedBitesBackend/internal/vertical/users/domain/entity"
 	"github.com/dgsaltarin/SharedBitesBackend/internal/vertical/users/domain/repository"
-	"github.com/google/uuid"
 )
 
 type userService struct {
@@ -20,8 +19,12 @@ func NewUserService(repository repository.UserRepository) services.UserService {
 }
 
 func (u *userService) SignUp(user entity.User) error {
-	user.ID = uuid.New().String()
-	err := u.repository.UpsertUser(&user)
+	newUser, err := entity.NewUser(user)
+	if err != nil {
+		fmt.Println("Error creating user: ", err)
+		return err
+	}
+	err = u.repository.UpsertUser(newUser)
 	if err != nil {
 		fmt.Println("Error signing up: ", err)
 		return err
